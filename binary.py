76eef255dca4911af664c36b38d844069894e094
Mark Sherrington <msherrington@users.noreply.github.com>
2019-10-13 14:21:04 +0100

    Added second, more efficient function

--- binary.py
+++ binary.py
@@ -4,3 +4,11 @@ def binary(n):
         array.append(str(n % 2))
         n = int(n // 2)
     print(''.join(list(reversed(array))))
+
+# more efficient function
+def binary(n):
+    string = ''
+    while n > 0:
+        string = str(n % 2) + string
+        n = int(n // 2)
+    print(string)
